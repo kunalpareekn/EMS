0291f4a14751cb3ed5525d78a245b095fa8eef4a
kunalpareekn <138710854+kunalpareekn@users.noreply.github.com>
2025-06-09 17:29:04 +0530

    Delete Api directory

--- Api/routes/authAdminroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.post('/register', registrationController.registerUser);
-router.post('/login', registrationController.loginUser);
-
-module.exports = router; 
