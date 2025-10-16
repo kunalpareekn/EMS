bfaf61382f711b07960c8ad979eeb487c586d1dc
kunalpareekn <138710854+kunalpareekn@users.noreply.github.com>
2025-10-16 16:13:12 +0530

    Update constant.js

--- vite_client/src/utils/constant.js
+++ vite_client/src/utils/constant.js
@@ -1,28 +1,17 @@
-
-
 //==================Admin===================//
-
-export const ADMIN_AUTH_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/admin/auth"
-export const ADMIN_LEAVE_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/admin/leave"
-export const ADMIN_PROJECT_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/admin/project"
-export const ADMIN_PAYROLL_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/admin/payroll"
-
+export const ADMIN_AUTH_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/admin/auth";
+export const ADMIN_LEAVE_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/admin/leave";
+export const ADMIN_PROJECT_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/admin/project";
+export const ADMIN_PAYROLL_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/admin/payroll";
 
 //==================Employee===================//
-
-export const EMPLOYEE_AUTH_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/employee/auth"
-export const EMPLOYEE_LEAVE_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/employee/leave"
-
+export const EMPLOYEE_AUTH_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/employee/auth";
+export const EMPLOYEE_LEAVE_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/employee/leave";
 
 //==================Both===================//
-export const BOTH_PROFILE_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/both/profile-details"
-export const BOTH_NOTIFICATION_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/both/notification"
-export const BOTH_ATTENDANCE_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/both/attendance"
-export const BOTH_DOCUMENT_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/both/document"
-export const BOTH_PASSWORD_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/both/password"
-export const BOTH_TASK_ENDPOINT="https://ems-frontend-steel-gamma.vercel.app/api/v1/both/project-task"
-
-
-
-
-
+export const BOTH_PROFILE_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/profile-details";
+export const BOTH_NOTIFICATION_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/notification";
+export const BOTH_ATTENDANCE_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/attendance";
+export const BOTH_DOCUMENT_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/document";
+export const BOTH_PASSWORD_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/password";
+export const BOTH_TASK_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/project-task";
